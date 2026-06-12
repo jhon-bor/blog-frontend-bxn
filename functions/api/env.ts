ee4a7bfa44450fa3255ecc39e8ec075241bea481
Jhon <jhon@192.168.0.104>
2026-06-12 13:20:19 +0800

    fix: add all missing Env properties

--- functions/api/env.ts
+++ functions/api/env.ts
@@ -2,7 +2,13 @@ export interface Env {
   DB: D1Database;
   NEXT_PUBLIC_BASE_URL: string;
   GOOGLE_ANALYTICS_TOKEN?: string;
-  CLOUDINARY_SECRET_NAME: string;
-  CLOUDINARY_SECRET_KEY: string;
-  CLOUDINARY_SECRET: string;
+  GA_PROPERTY_ID?: string;
+  GOOGLE_TRENDS_TOKEN?: string;
+  SERPAPI_TOKEN?: string;
+  GITHUB_TOKEN?: string;
+  CLOUDINARY_SECRET_NAME?: string;
+  CLOUDINARY_SECRET_KEY?: string;
+  CLOUDINARY_SECRET?: string;
+  GOOGLE_MEASUREMENT_ID?: string;
+  GA_API_SECRET?: string;
 }
