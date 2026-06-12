514eae26b4d7178942a5478af6c1102287e1515b
Jhon <jhon@192.168.0.104>
2026-06-12 13:17:31 +0800

    fix: add GOOGLE_ANALYTICS_TOKEN to Env

--- functions/api/env.ts
+++ functions/api/env.ts
@@ -1,6 +1,7 @@
 export interface Env {
   DB: D1Database;
   NEXT_PUBLIC_BASE_URL: string;
+  GOOGLE_ANALYTICS_TOKEN?: string;
   CLOUDINARY_SECRET_NAME: string;
   CLOUDINARY_SECRET_KEY: string;
   CLOUDINARY_SECRET: string;
